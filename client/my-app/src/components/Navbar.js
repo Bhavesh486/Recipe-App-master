85e4de088ef8292f1df6b393612af1c1f5df937b
Bhavesh Kulthe <bhavesh486@github.com>
2026-04-01 20:46:36 +0530

    fix unused variable error

--- client/my-app/src/components/Navbar.js
+++ client/my-app/src/components/Navbar.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "../App.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faRotate } from "@fortawesome/free-solid-svg-icons";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
